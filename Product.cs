ba8e1bfe2b459abc543c2b6742c7e5292e16d0c3
Toqeer Ahmad <136381451+toqeerabid10@users.noreply.github.com>
2024-05-19 02:17:29 +0500

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿namespace AgroEase1
+{
+    public class Product
+    {
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImagesUrl { get; set; }
+        public bool IsAvailable { get; set; }
+        public string condition { get; set; }
+        public  string category { get; set; }
+       public string quantity {get; set; }
+        
+    }
+    
+
+}
